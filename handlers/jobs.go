ffa060decab845fd34ba28df7a85c72476667828
Conrad Pankoff <deoxxa@fknsrs.biz>
2025-10-12 23:19:50 +1100

    implement all/running toggle for job view

--- handlers/jobs.go
+++ handlers/jobs.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"fknsrs.biz/p/sorm"
@@ -11,13 +12,31 @@ import (
 )
 
 func Jobs(rw http.ResponseWriter, r *http.Request) {
+	show := r.URL.Query().Get("show")
+	if show == "" {
+		show = "running"
+	}
+
+	where := ""
+
+	switch show {
+	case "all":
+		where = ""
+	case "running":
+		where = "where finished_at is null"
+	default:
+		panic(fmt.Errorf("invalid value for `show' parameter"))
+	}
+
 	var jobs []jobqueue.Job
-	if err := sorm.FindWhere(r.Context(), ctxdb.GetDB(r.Context()), &jobs, "where finished_at is null order by id desc limit 1500"); err != nil {
+	if err := sorm.FindWhere(r.Context(), ctxdb.GetDB(r.Context()), &jobs, where+" order by id desc limit 1500"); err != nil {
 		panic(err)
 	}
 
 	if err := ctxtemplate.ExecuteTemplateIntoResponse(r, rw, "page_jobs", map[string]interface{}{
-		"Jobs": jobs,
+		"Jobs":    jobs,
+		"Show":    show,
+		"Details": r.URL.Query().Get("details") != "",
 	}); err != nil {
 		panic(err)
 	}
